718c58bf6ee0f9cb72fdef81c7d5d0d4da11054b
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-24 20:10:44 +0200

    $0010: [SelectToken] [Modal]: render selected token on swap - from

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const serverUrl = "https://8inywvl84jj3.usemoralis.com:2053/server";
 const appId = "gRPRv2h84tVbuY12ounbiq0AtWUAXQPIiHu50jtX";
 
 let tokens;
+let currentTrade = {};
+let currentSelectSide;
 
 async function init() {
     Moralis.start({ serverUrl, appId });
@@ -12,35 +14,7 @@ async function init() {
     await Moralis.initPlugins();
     await Moralis.enableWeb3();
     await listAvailableTokens();
-   
 }
-
-async function listAvailableTokens() {
-    /* Get supported tokens */
-    const result = await Moralis.Plugins.oneInch.getSupportedTokens({
-        chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
-    });
-    console.log(result);
-
-    tokens = result.tokens;
-    let parent = document.getElementById("token_list");
-    for (const address in tokens) {
-      let token = tokens[address];
-      //build html and append it to token list
-      let div = document.createElement("div");
-      div.setAttribute("data-address", address);
-      div.className = "token_row";
-      let html = `
-          <img class="token_list_img" src="${token.logoURI}">
-          <span class="token_list_text">${token.symbol}</span>
-          `;
-      div.innerHTML = html;
-      div.onclick = () => {
-        selectToken(address);
-      };
-      parent.appendChild(div);
-    }
-  }
 
 /** Add from here down */
 async function login() {
@@ -61,12 +35,59 @@ async function logOut() {
   console.log("logged out");
 }
 
-
 /* ~~~~~~~~~~~~~~SELECT TOKEN MODAL~~~~~~~~~~~~~~ */
+
+
+async function listAvailableTokens() {
+    /* Get supported tokens */
+    const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+        chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+    });
+    console.log(result);
+
+    tokens = result.tokens;
+
+    let parent = document.getElementById("token_list");
+    for (const address in tokens) {
+      let token = tokens[address];
+      //build html and append it to token list
+      let div = document.createElement("div");
+      // emmbed address into element
+      div.setAttribute("data-address", address);
+      div.className = "token_row";
+      let html = `
+          <img class="token_list_img" src="${token.logoURI}">
+          <span class="token_list_text">${token.symbol}</span>
+          `;
+      div.innerHTML = html;
+      div.onclick = () => {
+        selectToken(address);
+      };
+      parent.appendChild(div);
+    }
+}
+
+function selectToken(address) {
+    closeSelectTokenModal();
+    console.log(address);
+
+    currentTrade[currentSelectSide] = tokens[address];
+    console.log(currentTrade);
+
+    renderInterface()
+}
+
+function renderInterface() {
+    if (currentTrade.from) {
+        document.getElementById("from_token_image").src = currentTrade.from.logoURI;
+        document.getElementById("from_token_text").innerHTML = currentTrade.from.symbol;
+    }
+}
 
 const token_modal = document.getElementById("token_modal");
 
-function openSelectTokenModal(){
+function openSelectTokenModal(side){
+    currentSelectSide = side;
     token_modal.style.display = "block";
 }
 
@@ -75,10 +96,9 @@ function closeSelectTokenModal(){
 }
 
 
-
 init();
 
-document.getElementById("from_token_select").onclick = openSelectTokenModal;
+document.getElementById("from_token_select").onclick = (() => { openSelectTokenModal("from")});
 document.getElementById("modal_close").onclick = closeSelectTokenModal;
 
 document.getElementById("btn-login").onclick = login;
