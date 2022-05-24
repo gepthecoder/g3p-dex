fa33a2582d1583824da298dd991eee2058d337fb
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-24 19:39:02 +0200

    $0008: [SelectToken] [Modal]: token list implementation

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
+/* ~~~~~~~~~~~~~~LOGIN / LOGOUT~~~~~~~~~~~~~~ */
+
 /** Connect to Moralis server */
 const serverUrl = "https://8inywvl84jj3.usemoralis.com:2053/server";
 const appId = "gRPRv2h84tVbuY12ounbiq0AtWUAXQPIiHu50jtX";
-Moralis.start({ serverUrl, appId });
+
+let tokens;
+
+async function init() {
+    Moralis.start({ serverUrl, appId });
+
+    await Moralis.initPlugins();
+    await Moralis.enableWeb3();
+    await listAvailableTokens();
+   
+}
+
+async function listAvailableTokens() {
+    /* Get supported tokens */
+    const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+        chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+    });
+    console.log(result);
+
+    tokens = result.tokens;
+    let parent = document.getElementById("token_list");
+    for (const address in tokens) {
+      let token = tokens[address];
+      //build html and append it to token list
+      let div = document.createElement("div");
+      div.setAttribute("data-address", address);
+      div.className = "token_row";
+      let html = `
+          <img class="token_list_img" src="${token.logoURI}">
+          <span class="token_list_text">${token.symbol}</span>
+          `;
+      div.innerHTML = html;
+      div.onclick = () => {
+        selectToken(address);
+      };
+      parent.appendChild(div);
+    }
+  }
 
 /** Add from here down */
 async function login() {
@@ -23,7 +62,7 @@ async function logOut() {
 }
 
 
-/* SELECT TOKEN MODAL */
+/* ~~~~~~~~~~~~~~SELECT TOKEN MODAL~~~~~~~~~~~~~~ */
 
 const token_modal = document.getElementById("token_modal");
 
@@ -36,8 +75,10 @@ function closeSelectTokenModal(){
 }
 
 
+
+init();
+
 document.getElementById("from_token_select").onclick = openSelectTokenModal;
 document.getElementById("modal_close").onclick = closeSelectTokenModal;
 
 document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
